d04fc07d7fdc8dce9dee32e56b58325bf277efd4
Marcus Moore <momoore1231@gmail.com>
2023-10-29 19:18:10 -0400

    CV-3: List Different Category Options

---
+++ Cineverse/Views/SubViews/ExploreSubViews/AToZView.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function AToZView() {
+  return (
+    <View>
+      <Text>A-Z View</Text>
+    </View>
+  );
+}
